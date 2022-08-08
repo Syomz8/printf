a2793dd9723500f3579837936ff2fad008046c90
Syomz8 <107145853+Syomz8@users.noreply.github.com>
2022-08-08 18:00:42 +0300

    Update handle_print.c
    Signed-off-by: Syomz8 <107145853+Syomz8@users.noreply.github.com>

--- handle_print.c
+++ handle_print.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments.
+ * handle_print - Prints an argument by inferring to type
+ * @fmt: Formatted string for printing arguments
  * @list: List of arguments to be printed.
  * @ind: ind.
  * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
+ * @flags: determine active flags
  * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
